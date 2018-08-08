a15c8b07c9649a9250c87ea9eb3a314318f75b09
Lucien Najev <luciennajev@hotmail.com>
2018-08-08 14:04:38 +0100

    no changes

--- spec/note_spec.rb
+++ spec/note_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Note do
 
   it "responds to see_note method" do
     expect(subject).to respond_to :see_note
+    # Alternative
+    # it { is_expected.to respond_to :see_note }
   end
 
 
