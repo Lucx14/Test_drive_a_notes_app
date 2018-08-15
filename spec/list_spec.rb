32e7808cad1b945057950f46ccdb35f13cb2df22
Lucien Najev <luciennajev@hotmail.com>
2018-08-15 11:29:42 +0100

    refactoring

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,14 +1,12 @@
 require 'list'
 
 describe List do
-
+  let(:note) { double :note }
   it "will create a new list instance as an empty array" do
-    new_list = List.new
-    expect(new_list.list).to eq([])
+    expect(subject.list).to eq([])
   end
 
   it ".add_note - will allow us to add note to the list" do
-    note = Note.new("title", "body")
     expect(subject.add_note(note)).to eq([note])
   end
 
